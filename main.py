3abb9890be35dfcf465fa6053713c4a21c9bcd38
ShayAry <104421168+ShayAry@users.noreply.github.com>
2022-11-28 17:30:04 +0200

    Update

--- main.py
+++ main.py
@@ -6,43 +6,44 @@ requests.packages.urllib3. \
     disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 # ===================== NVD SCAN ===========================
-oss_name = ''''''   # Can be one or multiple oss / one or more tuples of (vendor, product)
-cpe_name = ''''''   # E.g: "cpe:2.3:o:microsoft:windows_10:1607"
+oss_name = ''''''  # Can be one or multiple oss / one or more tuples of (vendor, product)
+cpe_name = ''''''  # E.g: "cpe:2.3:o:microsoft:windows_10:1607"
 nvd_token = ''''''  # API key to increase the rate limit.(optional)
+
+
 # ==========================================================
 
-
-def format_text(title, item):
-    cr = '\r\n'
-    section_break = cr + "*" * 20 + cr
-    item = str(item)
-    text = Style.BRIGHT + Fore.RED + title + Fore.RESET + section_break + item + section_break
-    return text
-
-def request(url):
-    global r
+def request():
     if oss_name:
+        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={oss_name}"
         if nvd_token:
             headers = {'apiKey': f"{nvd_token}"}
-            url_for_nvd_exact_match = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={oss_name}"
-            r = requests.get(url_for_nvd_exact_match, headers=headers)
-        return r
+            r = requests.get(url, headers=headers)
+            parsed_json = json.loads(r.content)
+            totalResults = parsed_json["totalResults"]
+            print(f"Total vulnerabilities found: {totalResults}.")
+            for cve in parsed_json["vulnerabilities"]:
+                print(cve['cve']["id"])
+        else:
+            r = requests.get(url)
+            parsed_json = json.loads(r.content)
+            totalResults = parsed_json["totalResults"]
+            print(f"Total vulnerabilities found: {totalResults}.")
+            for cve in parsed_json["vulnerabilities"]:
+                print(cve['cve']["id"])
 
-     if cpe_name:
-        url_for_nvd_cpe = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={cpe_name}"
-        r = requests.get(url_for_nvd_cpe)
+    if cpe_name:
+        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={cpe_name}"
+        r = requests.get(url)
+        parsed_json = json.loads(r.content)
+        totalResults = parsed_json["totalResults"]
+        print(f"Total vulnerabilities found: {totalResults}.")
+        for cve in parsed_json["vulnerabilities"]:
+            print(cve['cve']["id"])
 
-     else:
-         return "Required params missing"
+    else:
+        return "Required params missing"
+
 if __name__ == "__main__":
     request()
-    parsed_json = (json.loads(r.content))
-    totalResults = parsed_json["totalResults"]
-    cve = parsed_json["vulnerabilities"]
-
-print(f"Total vulnerabilities found: {totalResults}.")
-for cve in parsed_json["vulnerabilities"]:
-    print(cve['cve']["id"])
-
-
 
