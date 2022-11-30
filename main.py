bd62d7a28a8c8ffcf5412cd684339462a4cf616e
ShayAry <104421168+ShayAry@users.noreply.github.com>
2022-11-30 13:52:47 +0200

    Some changes

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
-import requests
-from colorama import Fore, Style
-import json
+import nvdlib
 
-requests.packages.urllib3. \
-    disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+# ================================================
+cve = ''''''
+prodcut = ''''''
+cpe = ''''''  # E.g: "cpe:2.3:a:microsoft:exchange_server:5.0:-:*:*:*:*:*:*"
+key = "" # KEY for NVD Api. (optional)
+wild_search = False
+# ================================================
+if cve != "" and not None:
+    r = nvdlib.searchCVE(cveId=cve, key=key, delay=1)[0]
+    print(str(r.v31severity) + ' - ' + str(r.v31score))
+    print("CVSS Score: " + r.v31vector)
+    print(r.descriptions[0].value)
+    print("Reported on: " + r.published)
+    print(r.url)
 
-# ===================== NVD SCAN ===========================
-oss_name = ''''''  # Can be one or multiple oss / one or more tuples of (vendor, product)
-cpe_name = ''''''  # E.g: "cpe:2.3:o:microsoft:windows_10:1607"
-nvd_token = ''''''  # API key to increase the rate limit.(optional)
-
-
-# ==========================================================
-
-def request():
-    if oss_name:
-        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={oss_name}"
-        if nvd_token:
-            headers = {'apiKey': f"{nvd_token}"}
-            r = requests.get(url, headers=headers)
-            parsed_json = json.loads(r.content)
-            totalResults = parsed_json["totalResults"]
-            print(f"Total vulnerabilities found: {totalResults}.")
-            for cve in parsed_json["vulnerabilities"]:
-                print(cve['cve']["id"])
-        else:
-            r = requests.get(url)
-            parsed_json = json.loads(r.content)
-            totalResults = parsed_json["totalResults"]
-            print(f"Total vulnerabilities found: {totalResults}.")
-            for cve in parsed_json["vulnerabilities"]:
-                print(cve['cve']["id"])
-
-    if cpe_name:
-        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={cpe_name}"
-        r = requests.get(url)
-        parsed_json = json.loads(r.content)
-        totalResults = parsed_json["totalResults"]
-        print(f"Total vulnerabilities found: {totalResults}.")
-        for cve in parsed_json["vulnerabilities"]:
-            print(cve['cve']["id"])
-
+if prodcut != "" and not None:
+    if wild_search:
+        q = nvdlib.searchCVE(keywordSearch=prodcut)
     else:
-        return "Required params missing"
+        q = nvdlib.searchCVE(keywordSearch=prodcut, keywordExactMatch=True)
+    for cve in q:
+        print(cve.id, str(cve.score[0]), cve.url)
 
-if __name__ == "__main__":
-    request()
-
+if cpe != "" and not None:
+    cves_list = nvdlib.searchCVE(cpeName=cpe)
+    for cve in cves_list:
+        print(cve.id + str(cve.score[0]), cve.url)
